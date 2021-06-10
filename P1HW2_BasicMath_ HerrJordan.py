0dd061169fe881e5bb9a9235fc61e2adf0fcac45
Herrj3026 <70588330+Herrj3026@users.noreply.github.com>
2021-06-10 13:21:12 -0400

    Add files via upload

---
+++ P1HW2_BasicMath_ HerrJordan.py
@@ -0,0 +1,19 @@
+#
+#6/10/2021
+#CTI-110 P1HW2 - Basic Math
+#Jordan Herr
+#
+
+#getting inou from user
+num_one = int(input("Please enter your first number: "))
+num_two = int(input("Please enter your second number: "))
+#quick maths
+sum_of = num_one+num_two
+multi_of = num_one*num_two
+
+## outputs
+print ("First number entered: ", num_one)
+print ("First number entered: ", num_two)
+print()
+print ("Sum of both numbers: ", sum_of)
+print("Result of multiplying both numbers: ", multi_of) 
