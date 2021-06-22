53df4ca49fb018035342d91f6a8390acc90e69e6
Herrj3026 <70588330+Herrj3026@users.noreply.github.com>
2021-06-22 12:51:36 -0400

    Add files via upload

---
+++ P2HW1_DistanceTraveled_HerrJordan.py
@@ -0,0 +1,22 @@
+#
+# A program to do a basic calcuation of how far you travled
+# 6/2220201
+# CTI-110 P2HW1 - DistanceTraveled
+# Jordan Herr
+#
+
+
+#section for the inputs
+car_speed = float(input("Please enter car speed: "))
+time_travled = float(input("Please enter distance travled: "))
+
+#math section for the calculations
+dis_travled = car_speed * time_travled
+
+
+#section for the output
+print()#white space to make it neat
+print("Speed entered:", car_speed)
+print("Time entered:", time_travled)
+print()#white space to make it neat
+print("Distance Travled", dis_travled)
